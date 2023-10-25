0b12f9bd9022f4bd145ccea115620c061f459ab0
Dektarion <andreiy.boyarsky@gmail.com>
2023-10-25 09:06:09 +0300

    feat: done task

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -369,66 +369,37 @@ function getDigitalRoot(num) {
  *   '{)' = false
  *   '{[(<{[]}>)]}' = true
  */
-function isBracketsBalanced(/* str */) {
-  throw new Error('Not implemented');
-  // const closeDicr = {
-  //   '[': ']',
-  //   '(': ')',
-  //   '{': '}',
-  //   '<': '>',
-  // };
+function isBracketsBalanced(str) {
+  const openBrackets = ['[', '(', '{', '<'];
+  const pairBrackets = {
+    ']': '[',
+    ')': '(',
+    '}': '{',
+    '>': '<',
+  };
 
-  // const resArr = [];
+  const stack = [];
 
-  // for (let i = 0; i < str.length; i++) {
-  //   const currentChar = str[i];
-  //   if (Object.keys(closeDicr).includes(currentChar)) {
-  //   } else {
-  //     return false;
-  //   }
-  // }
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+    if (openBrackets.includes(currentChar)) {
+      stack.push(currentChar);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
 
-  // return resArr;
+      // eslint-disable-next-line prefer-const
+      let topElement = stack[stack.length - 1];
 
-
-  // let openArr = [];
-  // let closeDicr = {};
-  // let cloneBrackets = [];
-
-  // bracketsConfig.forEach((element) => {
-  //   openArr.push(element[0]);
-  //   closeDicr[element[1]] = element[0];
-  //   if (element[0] === element[1]) {
-  //     cloneBrackets.push(element[0])
-  //   }
-  // })
-
-  // let stack = [];
-  // for (let i = 0; i < str.length; i++){
-  //   let currentChar = str[i];
-  //   let topElement = stack[stack.length - 1];
-
-  //   if (cloneBrackets.includes(currentChar) && stack.includes(currentChar)) {
-  //     if (currentChar === topElement) {
-  //       stack.pop(currentChar);
-  //       } else {
-  //         return false;
-  //       }
-  //     } else if (openArr.includes(currentChar)){
-  //         stack.push(currentChar);
-  //     } else {
-  //         if (stack.length === 0){
-  //         return false;
-  //         }
-
-  //         if (closeDicr[currentChar] === topElement){
-  //           stack.pop(currentChar);
-  //         } else {
-  //           return false;
-  //         }
-  //     }
-  // }
-  // return stack.length === 0;
+      if (pairBrackets[currentChar] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
 
 
